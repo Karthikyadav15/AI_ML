58bb93138e6839a26e49fcd666328238befa8766
Karthikyadav15 <55484915+Karthikyadav15@users.noreply.github.com>
2022-03-16 20:21:10 +0530

    Update PGM8.py

--- PGM8.py
+++ PGM8.py
@@ -1,9 +1,4 @@
-# Python program to demonstrate # KNN classification algorithm # on IRISdataset
-#Write a program to implement k-Nearest Neighbour algorithm to classify the iris data set.
-#Print both correct and wrong predictions. Java/Python ML library classes can be used for
-#this problem.
 
-#import the dataset and library files
 from sklearn.datasets import load_iris
 from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
@@ -11,14 +6,14 @@ from sklearn.model_selection import train_test_split
 
 iris_dataset=load_iris()
 
-#display the iris dataset
+
 print("\n IRIS FEATURES \ TARGET NAMES: \n ", iris_dataset.target_names)
 for i in range(len(iris_dataset.target_names)):
     print("\n[{0}]:[{1}]".format(i,iris_dataset.target_names[i]))
 
 print("\n IRIS DATA :\n",iris_dataset["data"])
 
-#split the data into training and testing data
+
 X_train, X_test, y_train, y_test = train_test_split(iris_dataset["data"], iris_dataset["target"], random_state=0)
 
 print("\n Target :\n",iris_dataset["target"])
@@ -27,11 +22,11 @@ print("\n X TEST \n", X_test)
 print("\n Y TRAIN \n", y_train)
 print("\n Y TEST \n", y_test)
 
-#train and fit the model
+
 kn = KNeighborsClassifier(n_neighbors=5)
 kn.fit(X_train, y_train)
 
-#predicting from model
+
 x_new = np.array([[5, 2.9, 1, 0.2]])
 print("\n XNEW \n",x_new)
 prediction = kn.predict(x_new)
